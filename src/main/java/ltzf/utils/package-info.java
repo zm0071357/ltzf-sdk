adc2527ada646bd7f3ef42fc0b1aeb8dd4f1fc5f
zm0071357 <834302225@qq.com>
2024-11-09 14:11:31 +0800

    feat:分析和验证支付接口

--- src/main/java/ltzf/utils/package-info.java
+++
@@ -1,1 +0,0 @@
-package ltzf.utils;
--- src/test/java/ltzf/test/ApiTest.java
+++ src/test/java/ltzf/test/ApiTest.java
@@ -1,7 +1,23 @@
 package ltzf.test;
 
+import ltzf.utils.SignUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApiTest {
     public static void main(String[] args) {
-        System.out.println("test");
+        String key = "92e72771d4d3e75de9086c2e5e2a27e3";
+        long timestamp = System.currentTimeMillis() / 1000;
+        System.out.println(timestamp);
+        Map<String,String> dataMap = new HashMap<>();
+        dataMap.put("mch_id","1698731613");
+        dataMap.put("out_trade_no","LTZF2022113023096");
+        dataMap.put("total_fee","0.01");
+        dataMap.put("body","QQ公仔");
+        dataMap.put("timestamp", String.valueOf(timestamp));
+        dataMap.put("notify_url","http://xiaoming-programming.top");
+
+        System.out.println(SignUtils.createSign(dataMap, key));
     }
 }
