0b26d25deb9bd92e812cd299d4ab965a7c2eedd4
zm0071357 <834302225@qq.com>
2024-11-09 14:11:31 +0800

    feat:分析和验证支付接口

---
+++ docs/api/native.sh
@@ -0,0 +1,3 @@
+curl -X POST "https://api.ltzf.cn/api/wxpay/native" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "mch_id=1698731613&out_trade_no=LTZF2022113023096&total_fee=0.01&body=QQ公仔&timestamp=1669533132&notify_url=http://xiaoming-programming.top&sign=B7337098E280841EB5F4D28261B60C07"
